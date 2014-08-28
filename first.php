66a691f051e19d69d3ca001cb110967f0088b670
Iceshadoz <c_vasquez257@yahoo.com>
2014-08-28 13:53:23 -0800

    Signed-off-by: Iceshadoz <c_vasquez257@yahoo.com>

---
+++ first.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C// DTD XHTML 1.0 Transitional//EN" 
+    "http://www.w3.org/TR/xhtml1/DTD/ xhtml1-transitional.dtd">
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html xmlns="http://www.w3.org/ 
+1999/xhtml" xml:lang="en" 
+lang="en">
+    <head>
+        <meta  http-equiv="content-type" content="text/html; charset="UTF-8" />
+        <title>Page Title</title>
+    </head>
+    <body>
+        <!-- Script 1.2 - first.php -->
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
